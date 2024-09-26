1a74f45a6b7ea7b6eadef2f21383407e5c54c6e4
NAMAN BATRA <naman@Namans-MacBook-Air.local>
2024-09-26 08:02:37 +0530

    minor change

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 h-4 text-white">
-        © 2024 True Feedback. All rights reserved.
+        ♥️ Naman Batra
       </footer>
     </>
   );
